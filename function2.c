25fbcd9ea2186080c463a240190f9f92a1e602ca
AymanAegon <ayo12ammar24@gmail.com>
2023-06-22 15:00:40 -0700

    5. nope

--- function2.c
+++ function2.c
@@ -97,3 +97,14 @@ void add_func(stack_t **head, unsigned int line_number)
 	*head = h->next;
 	free(h);
 }
+/**
+  *nop_func - nothing
+  *@head: stack head
+  *@line_number: line_number
+  *Return: no return
+ */
+void nop_func(stack_t **head, unsigned int line_number)
+{
+	(void) counter;
+	(void) head;
+}
